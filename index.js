0c2f3eca11b0e23e4e798e1cb08a36826f719a18
Hannah Alverson <alverson98@yahoo.com>
2022-08-27 14:32:23 -0600

    added packages, function to make README file, and function to initialize app

--- index.js
+++ index.js
@@ -1,77 +1,96 @@
 // TODO: Include packages needed for this application
 
+const fs = require("fs");
+const inquirer = require("inquirer");
+
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: "input",
-        message: "What is the title of your project?",
-        name: "title",
-    }, {
-        type: "input",
-        message: "Please provide a description of your project.",
-        name: "description",
-    }, {
-        type: "input",
-        message: "How do you install your project?",
-        name: "install",
-    }, {
-        type: "input",
-        message: "How do you use your project?",
-        name: "usage",
-    }, {
-        type: "checkbox",
-        message: "What licenses does your project have?",
-        //choices: //put choices here!!
-        name: "license",
-    }, {
-        type: "input",
-        message: "How would you like others to contribute?",
-        name: "contribute",
-    },  {
-        type: "input",
-        message: "Please provide instructions of how to run the tests for this project.",
-        name: "test",
-    },{
-        type: "input",
-        message: "What is your full name?",
-        name: "name",
-    },{
-        type: "input",
-        message: "What is your email address?",
-        name: "email",
-    },{
-        type: "input",
-        message: "What is your GitHub profile URL?",
-        name: "gitHub",
-    },{
-        type: "input",
-        message: "Please specify how to reach you for additional questions.",
-        name: "question",
-    }
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Please provide a description of your project.",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "How do you install your project?",
+    name: "install",
+  },
+  {
+    type: "input",
+    message: "How do you use your project?",
+    name: "usage",
+  },
+  {
+    type: "checkbox",
+    message: "What licenses does your project have?",
+    //choices: //put choices here!!
+    name: "license",
+  },
+  {
+    type: "input",
+    message: "How would you like others to contribute?",
+    name: "contribute",
+  },
+  {
+    type: "input",
+    message:
+      "Please provide instructions of how to run the tests for this project.",
+    name: "test",
+  },
+  {
+    type: "input",
+    message: "What is your full name?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is your GitHub profile URL?",
+    name: "gitHub",
+  },
+  {
+    type: "input",
+    message: "Please specify how to reach you for additional questions.",
+    name: "question",
+  },
 ];
 
+const fileName = "README.md";
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, fileContents) {
+  fs.writeFile(fileName, JSON.stringify(fileContents));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions);
+}
 
 // Function call to initialize app
 init();
 
-
 // ACCEPTANCE CRITERIA
 // - WHEN I am prompted for information about my application repository
-        // THEN a high-quality, professional   README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// THEN a high-quality, professional   README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 // - WHEN I enter my project title
-        // THEN this is displayed as the title of the README
+// THEN this is displayed as the title of the README
 // - WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-        // THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
 // - WHEN I choose a license for my application from a list of options
-        // THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
 // - WHEN I enter my GitHub username
-        // THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
 // - WHEN I enter my email address
-        // THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
 // - WHEN I click on the links in the Table of Contents
-        // THEN I am taken to the corresponding section of the README
+// THEN I am taken to the corresponding section of the README
